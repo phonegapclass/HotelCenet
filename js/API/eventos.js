daa7413e098b67285bbb736cb685e3cfbe5be6a4
Carlos Celma <solrac@igitsoft.com>
2013-10-19 13:19:29 -0500

    kjh

---
+++ js/API/eventos.js
@@ -0,0 +1,30 @@
+//eventos
+$(function(){
+	document.addEventListener("deviceready", function(){
+		$('#regEnv').click(function(){
+			var nom = $('#regNom').val();
+			var mail = $('#regEma').val();
+			var tel = $('#regTel').val();
+			
+			if(nom != '' && mail != '' && tel != ''){
+				navigator.notification.confirm(nom+'\n'+mail+'\n'+tel,function(btn){
+					switch(btn){
+						case 1:
+							navigator.notification.beep(2);
+							break;
+						case 2:
+							navigator.notification.vibrate(500);
+							break;
+					}
+				},'Datos de Registro','Beep,Vibrar,Cancelar');	
+			}else{
+				navigator.notification.alert('Todos los campos son requeridos',null,'Error','Aceptar');
+			}
+		});
+	}, false);
+});
+
+/*(function jQuery(selector){
+	...
+	return obj;
+})$;*/
